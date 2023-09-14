ed8298e57c9e05767dbb93972f9357a628df4034
Francisco Berti da Cruz <fbc991@live.com>
2023-09-14 11:36:19 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
 const App = () => {
   return (
     <>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
       <ToastContainer
         position="top-center"
         style={{ textTransform: 'capitalize' }}
