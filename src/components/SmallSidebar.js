afb8d60c499e9fa325c3005077e7efc2eff9a79a
Francisco Berti da Cruz <fbc991@live.com>
2023-09-15 16:21:50 -0300

    Navlinks

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,7 +1,51 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import Logo from './Logo';
+import { toggleSidebar } from '../features/user/userSlice';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
 
 const SmallSidebar = () => {
-  return <Wrapper>SmallSidebar</Wrapper>;
+  const dispatch = useDispatch();
+  const { isSidebarOpen } = useSelector((store) => store.userState);
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
+  return (
+    <Wrapper>
+      <div
+        className={`sidebar-container ${isSidebarOpen ? 'show-sidebar' : ''}`}
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { id, text, path, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  key={id}
+                  className="nav-link"
+                  onClick={toggle}
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
