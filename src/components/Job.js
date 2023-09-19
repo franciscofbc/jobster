8380da59b39bcdbbff09b3f1df99e6738408ce92
Francisco Berti da Cruz <fbc991@live.com>
2023-09-18 23:28:24 -0300

    ...

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,4 +1,53 @@
-const Job = () => {
-  return <div>Job</div>;
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import Wrapper from '../assets/wrappers/Job';
+import { Link } from 'react-router-dom';
+
+const Job = ({
+  _id,
+  position,
+  company,
+  jobLocation,
+  jobType,
+  createdAt,
+  status,
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <Wrapper>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className="content">
+        <div className="content-center">
+          <h4>more content</h4>
+          <div className={`status ${status}`}>{status}</div>
+        </div>
+        <footer>
+          <div className="actions">
+            <Link
+              to="/add-job"
+              className="btn edit-btn"
+              onClick={() => console.log('edit')}
+            >
+              edit
+            </Link>
+            <button
+              type="button"
+              className="btn delete-btn"
+              onClick={() => console.log('delete')}
+            >
+              delete
+            </button>
+          </div>
+        </footer>
+      </div>
+    </Wrapper>
+  );
 };
 export default Job;
